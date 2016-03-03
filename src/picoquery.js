6abfe353c2444f39f9f5d557b63608c8ce696419
Bjørn Rosell <it@rosell.dk>
2016-03-03 09:58:27 +0100

    Create picoquery.js

---
+++ src/picoquery.js
@@ -0,0 +1,35 @@
+function picoQuery(sel) {
+  return new picoQueryClass(sel);
+}
+function picoQueryClass(sel) {
+
+  if (typeof sel === 'string') { 
+    this.nodes = Array.prototype.slice.call(document.querySelectorAll(sel));
+  }
+  // Handle $(DOMElement)
+  else if (sel.nodeType ) {
+    this.nodes = [sel];
+  }
+  // Handle $(DOMElementList)
+  else if (sel.length) {
+    this.nodes = Array.prototype.slice.call(sel);
+  }
+}
+picoQueryClass.prototype = {  
+  each: function(fn) {
+    [].forEach.call(this.nodes, fn);
+  },
+  get: function(i) {
+    return this.nodes[i];
+  },
+  addClass: function (v) {
+    return this.each(function (n) {
+      if (n.classList) {
+        n.classList.add(v);
+      } 
+      else {
+        n.className += ' ' + v;
+      }
+    });
+  },
+}
