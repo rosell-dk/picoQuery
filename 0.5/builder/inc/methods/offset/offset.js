9d261d6724d64546d5bf84afcf729cd970b6a6f5
Bjørn Rosell <it@rosell.dk>
2016-11-29 18:10:20 +0100

    improved compatibility

--- 0.5/builder/inc/methods/offset/offset.js
+++ 0.5/builder/inc/methods/offset/offset.js
@@ -1,24 +1,84 @@
-/*
-.offset() 
-
-Description:
-  Get the current coordinates of the first element, or set the coordinates of every element, in the set of matched elements, relative to the document.
-  http://api.jquery.com/offset/
-
-Supported signatures:   
-  .offset( ) => Object
-
-Unsupported signatures:
-  .offset( coordinates ) => jQuery
-  .offset( function ) => jQuery
-
-*/
-
 offset: function(a) {
-  var rect = this.e[0].getBoundingClientRect();
-  return {
-    top: rect.top + window.pageYOffset,
-    left: rect.left + window.pageXOffset
+  if (a) {
+    // SET
+    __ITERATE__(<@ this.e @>, <@ function(el,i) {
+      
+      var curPosition, curLeft, curCSSTop, curTop, curOffset, curCSSLeft, calculatePosition,
+	      curElem = $(el),
+	      position = curElem.css( "position" ),
+	      props = {};
+
+      if ( position === "static" ) {
+	      el.style.position = "relative";
+      }
+
+      curOffset = curElem.offset();
+      curCSSTop = curElem.css( "top" );
+      curCSSLeft = curElem.css( "left" );
+      calculatePosition = ( position === "absolute" || position === "fixed" ) && (curCSSTop == "auto" || curCSSLeft == "auto");
+
+      if ( calculatePosition ) {
+	      curPosition = curElem.position();
+	      curTop = curPosition.top;
+	      curLeft = curPosition.left;
+      } else {
+	      curTop = parseFloat( curCSSTop ) || 0;
+	      curLeft = parseFloat( curCSSLeft ) || 0;
+      }
+
+	    if ( __IS_FUNCTION__(<@ a @>) ) {
+
+		    // Use jQuery.extend here to allow modification of coordinates argument (gh-1848)
+//		    a = a.call( elem, i, jQuery.extend( {}, curOffset ) );
+        a = a.call( elem, i, curOffset);
+	    }
+
+	    if ( a.top != null ) {
+		    props.top = ( a.top - curOffset.top ) + curTop;
+	    }
+	    if ( a.left != null ) {
+		    props.left = ( a.left - curOffset.left ) + curLeft;
+	    }
+
+	    if ( "using" in a ) {
+		    a.using.call( elem, props );
+	    } else {
+		    curElem.css( props );
+	    }
+
+    } @>);
+
+    return this;
+
+  }
+  else {
+    // GET
+
+    // connected to dom, and not root?
+    if (this.e[0].compareDocumentPosition(this.e[0].ownerDocument.documentElement) & 8) {
+/*      var rect = this.e[0].getBoundingClientRect(),
+        win = this.e[0].ownerDocument.defaultView;
+      return {
+  			top: rect.top  + win.pageYOffset,
+  			left: rect.left + win.pageXOffset
+      }*/
+
+    var rect = this.e[0].getBoundingClientRect();
+    var elem = this.e[0],
+		  doc = elem && elem.ownerDocument,
+	    docElem = doc.documentElement,
+      win = doc.defaultView || elem.parentWindow;
+
+    return {
+			top: rect.top  + ( win.pageYOffset || docElem.scrollTop )  - ( docElem.clientTop  || 0 ),
+			left: rect.left + ( win.pageXOffset || docElem.scrollLeft ) - ( docElem.clientLeft || 0 )
+    }
+
+
+    }
+    else {
+      return { top: 0, left: 0 };
+    }
   }
 }
 
